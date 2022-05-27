6d8e67c6fece38daeb59e988ff19832ea439e522
Hassan Sultan Khan <hassansultankhan@gmail.com>
2022-05-27 15:32:11 +0500

    initiate

---
+++ main.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main() {
+  int loop;
+  var remainder;
+  var defaultValue;
+  var input = int.tryParse(stdin.readLineSync()!) ?? defaultValue;
+  if (input == defaultValue) {
+    print("Please enter a correct value");
+    main();
+  }
+  else{
+    var quotient = input ~/ 2;
+    for (loop = 2; loop <= quotient; loop++) {
+      remainder = input % loop;
+      if (remainder == 0) {
+        print("entered number is not a prime number");
+        print("Remainder:$remainder\nQuotient:$quotient");
+        return;
+      } else {
+        print('entered number is a prime number');
+        remainder = input % loop;
+        quotient = input ~/ 2;
+        print("Remainder:$remainder\nQuotient:$quotient");
+        return;
+      }
+    }
+    print("$input is prime number");
+  }
+}
